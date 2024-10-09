893dc3a522004f36066f1a0932b4d9a175fea05c
SatveerSingh777 <satveersinghsingh777@gmail.com>
2024-10-09 23:54:02 +0530

    Update SearchBar.js

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SearchBar(props) {
     return (
         <div >
-            <div className="card-header my-3">
+            <div className="card-header my-3" style={{width:"100%"}}>
                 <form className="d-flex" onSubmit={props.handleSubmit}>
                     <input
                         className="form-control me-2"
